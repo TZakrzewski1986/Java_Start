47be2be0d4a86010f1835a1dc3d153c98cdf37c0
Tomasz Zakrzewski <tomasz.zakrzewski86@gmail.com>
2020-08-03 13:52:41 +0200

    commit 2

---
+++ src/main/java/variables_and_data_types/ex_1/MainClass.java
@@ -0,0 +1,7 @@
+package variables_and_data_types.ex_1;
+
+public class MainClass {
+    public static void main(String[] args) {
+        System.out.printf("%.2f + %.2f + %.2f = %.2f",2.05,3.14,2.16,(2.05 + 3.14 + 2.16));
+    }
+}
