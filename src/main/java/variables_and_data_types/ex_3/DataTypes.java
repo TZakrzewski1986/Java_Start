954d800bb206d80b0d3ecaa3840c1af69c657f46
Tomasz Zakrzewski <tomasz.zakrzewski86@gmail.com>
2020-08-03 14:46:50 +0200

    commit 4

---
+++ src/main/java/variables_and_data_types/ex_3/DataTypes.java
@@ -0,0 +1,10 @@
+package variables_and_data_types.ex_3;
+
+public class DataTypes {
+    public static void main(String[] args) {
+        double x = 10;
+        double y = 4;
+        double div = x / y;
+        System.out.println(div);
+    }
+}
---
+++ src/main/java/variables_and_data_types/ex_4/DataTypesUnicode.java
@@ -0,0 +1,13 @@
+package variables_and_data_types.ex_4;
+
+public class DataTypesUnicode {
+    public static void main(String[] args) {
+        char k = 0x004B;
+        char o = 0x004F;
+        char t = 0x0054;
+
+        System.out.print(k);
+        System.out.print(o);
+        System.out.print(t);
+    }
+}
