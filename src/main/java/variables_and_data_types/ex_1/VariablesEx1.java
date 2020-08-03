e98aec6f10277c14d9c56d1058f85c8256a651a5
Tomasz Zakrzewski <tomasz.zakrzewski86@gmail.com>
2020-08-03 14:22:51 +0200

    commit 3

--- src/main/java/variables_and_data_types/ex_1/VariablesEx1.java
+++ src/main/java/variables_and_data_types/ex_1/VariablesEx1.java
@@ -1,6 +1,6 @@
 package variables_and_data_types.ex_1;
 
-public class MainClass {
+public class VariablesEx1 {
     public static void main(String[] args) {
         System.out.printf("%.2f + %.2f + %.2f = %.2f",2.05,3.14,2.16,(2.05 + 3.14 + 2.16));
     }
---
+++ src/main/java/variables_and_data_types/ex_2/VariablesEx2.java
@@ -0,0 +1,13 @@
+package variables_and_data_types.ex_2;
+
+public class VariablesEx2 {
+    public static void main(String[] args) {
+        double a = 2.50;
+        double b = 3.14;
+        double c = 4.445;
+        double result = a + b + c;
+
+        System.out.printf("%.2f + %.2f + %.2f = %.2f",a,b,c,result);
+    }
+
+}
